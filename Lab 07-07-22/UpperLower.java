315b444e5d2cdcc9c5964732ba3ab3659ced6738
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-07-07 17:48:53 +0530

    Create UpperLower.java
    To check whether a character is lowercase or uppercase.

---
+++ Lab 07-07-22/UpperLower.java
@@ -0,0 +1,14 @@
+package basicpackage;
+
+public class UpperLower {
+
+	public static void main(String[] args) {
+		char c='B';
+		if(c>='a' && c<='z')
+			System.out.println(c+" is lowercase.");
+		else
+			System.out.println(c+" is a uppercase.");
+
+	}
+
+}
