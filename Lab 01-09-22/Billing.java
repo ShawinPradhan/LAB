653b9f272a9bb35b0d515bae9318a9fb2c6c057d
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-09-02 00:42:49 +0530

    Create Billing.java

---
+++ Lab 01-09-22/Billing.java
@@ -0,0 +1,51 @@
+package restarraylist;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Billing {
+	static ArrayList<Bill> list = new ArrayList<Bill>();
+	static Scanner sc = new Scanner(System.in);
+	
+	
+	static void order(String fname, int Amt)
+	{
+		System.out.println("Enter quantity for "+fname+":");
+		int qty = sc.nextInt();
+		list.add(new Bill(fname,qty,qty*Amt));
+	}
+	
+	static void totalBill()
+	{
+		System.out.println("BILL");
+		System.out.println("=====");
+		double total = 0.0;
+		for(Bill i: list)
+		{
+			System.out.println(i.fname+"\t"+i.qty+"\t"+i.amt);
+			total += i.amt;
+		}
+		
+		System.out.println();
+		System.out.println("Amount:\tRs. "+total);
+		double gst = total*0.05;
+		System.out.println("GST:\tRs. "+gst);
+		System.out.println("--------------------");
+		double ftotal= total+gst;
+		System.out.println("Total:\tRs. "+ftotal);
+	}
+	
+	static void cont()
+	{
+		System.out.println("Do you want to continue? y/n");
+		char ch = sc.next().charAt(0); //taking the next character
+		if(ch!='y') //if the char is not y or is n then the loop gets run 
+		{
+			System.out.println();
+			totalBill(); //we get the bill
+			System.exit(0); //exit
+		}
+		System.out.println();
+	}
+	
+	}
