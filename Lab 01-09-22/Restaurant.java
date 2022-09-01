b6af8d35308db097ed891953b7aaa90c9c83a631
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-09-02 00:42:02 +0530

    Create Restaurant.java

---
+++ Lab 01-09-22/Restaurant.java
@@ -0,0 +1,46 @@
+package restarraylist;
+
+public class Restaurant {
+	String fname;
+	int price;
+	
+
+
+	public String getFname() {
+		return fname;
+	}
+
+
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+
+	public Restaurant(String fname, int price) {
+		super();
+		this.fname = fname;
+		this.price = price;
+	}
+
+
+
+	public void display()
+	{
+		System.out.println(" "+getFname()+"\t"+getPrice());
+	}
+	
+}
