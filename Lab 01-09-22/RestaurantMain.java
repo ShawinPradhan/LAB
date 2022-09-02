b9370337e7bbbfb784d73ffaf9246b272f7028a0
Shawin Pradhan <62380278+ShawinPradhan@users.noreply.github.com>
2022-09-02 14:33:51 +0530

    Update RestaurantMain.java

--- Lab 01-09-22/RestaurantMain.java
+++ Lab 01-09-22/RestaurantMain.java
@@ -43,37 +43,10 @@ public class RestaurantMain {
 		do {
 			System.out.println("Please enter food id:");
 			id = sc.nextInt();
-			switch(id)
-			{
-			case 1:
-				Restaurant rest = (Restaurant) map.get(1);
-				Billing.order(rest.getFname(), rest.getPrice());
-				break;
-			
-			case 2:
-				Restaurant rest1 = (Restaurant) map.get(2);
-				Billing.order(rest1.getFname(), rest1.getPrice());
-				break;
-				
-			case 3:
-				Restaurant rest3 = (Restaurant) map.get(3);
-				Billing.order(rest3.getFname(), rest3.getPrice());
-				break;
-				
-			case 4:
-				Restaurant rest4 = (Restaurant) map.get(4);
-				Billing.order(rest4.getFname(), rest4.getPrice());
-				break;
-				
-			case 5:
-				Restaurant rest5 = (Restaurant) map.get(5);
-				Billing.order(rest5.getFname(), rest5.getPrice());
-				break;
-				
-			default:
-				System.out.println("Wrong Input!!");
-			}
-			Billing.cont();
+			Restaurant rest = (Restaurant) map.get(id);
+			Billing.order(rest.getFname(), rest.getPrice());
+		
+		Billing.cont();
 			
 		}while(true);
 
